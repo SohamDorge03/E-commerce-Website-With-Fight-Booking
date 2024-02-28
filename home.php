79455b93ffcfd3de0e63625b284c1b66ba14ac1f
sohamdorge45@gmail.com <sohamdorge45@gmail.com>
2024-02-28 15:13:04 +0530

    Auto commit - Changes made

--- home.php
+++ home.php
@@ -2,31 +2,8 @@
 session_start();
 
 include("./include/connection.php");
-
-if (!isset($_SESSION['user_id'])) {
-    // Redirect to login page if user_id is not set
-    header("Location: login.php");
-    exit();
-}
-
-$user_id = $_SESSION['user_id'];
-
-// Query to check if the user's email is verified
-$emailVerificationQuery = "SELECT confirmed_email FROM users WHERE user_id = $user_id";
-$emailVerificationResult = mysqli_query($conn, $emailVerificationQuery);
-
-if ($emailVerificationResult) {
-    $row = mysqli_fetch_assoc($emailVerificationResult);
-
-    // If email is not verified, redirect to verification page
-    if (!$row['confirmed_email']) {
-        header("Location: verify.php");
-        exit();
-    }
-} else {
-    echo "Error: " . $emailVerificationQuery . "<br>" . mysqli_error($conn);
-}
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
